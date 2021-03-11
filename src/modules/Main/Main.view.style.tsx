9cbf50ff7b59a312eda3442617fee9dd69aac7eb
Lensom <magurawork@gmail.com>
2021-03-11 22:46:15 +0300

    init commit

---
+++ src/modules/Main/Main.view.style.tsx
@@ -0,0 +1,9 @@
+import { Platform, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22
+  }
+})
+
+export default styles;
---
+++ src/modules/Main/Main.view.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+import styles from './Main.view.style';
+
+const Main = () => {
+  return (
+    <View>
+      <Text style={styles.title}>
+        Main page
+      </Text>
+    </View>
+  )
+}
+
+export default Main;
